c4f628b9c942b01bb0490a94b0394b904af68e6a
shunich <fukusyun3@gmail.com>
2022-12-19 23:16:54 +0900

    add: test

---
+++ spec/requests/posts_controller_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+RSpec.describe 'Api::PostsController', type: :request do
+  describe 'GET /posts' do
+    before do
+      create_list(:post, 3)
+    end
+    let!(:path) { posts_path }
+
+    subject do
+      get path
+    end
+
+    context '正常系' do
+      it '200レスポンスが返ること' do
+        subject
+        expect(response).to have_http_status :ok
+      end
+      it '投稿データが取得できること' do
+        subject
+        res = JSON.parse(response.body)
+        expect(res.length).to eq(3)
+      end
+    end
+  end
+end
